5b7759ab8296302ac856ad63aac8477692a58953
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2022-12-09 16:51:52 -0600

    changes

--- database/database.js
+++ database/database.js
@@ -1,10 +1,20 @@
 import Sequelize from "sequelize";
 import mongoose, {connect} from "mongoose";
 
-export const sequelize = new Sequelize("bradescard", "root", "Password", {
-	host: "localhost",
-	dialect: "mysql",
-});
+// export const sequelize = new Sequelize("bradescard", "root", "Password", {
+// 	host: "localhost",
+// 	dialect: "mysql",
+// });
+
+export const sequelize = new Sequelize(
+	"Complianceapp",
+	"root",
+	"Bradescard#AWS2022#",
+	{
+		host: "bradescarddb.c8rvxzpmqq06.us-east-1.rds.amazonaws.com",
+		dialect: "mysql",
+	}
+);
 
 export const mongo = mongoose
 	.connect("mongodb://127.0.0.1:27017/compliance")
